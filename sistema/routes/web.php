0f587c00e6c4ad2732446b4f60980efa91270919
AlejandraHernandez1306 <175258924+AlejandraHernandez1306@users.noreply.github.com>
2026-01-28 01:06:51 -0600

    cambios

--- sistema/routes/web.php
+++ sistema/routes/web.php
@@ -1,10 +1,11 @@
 <?php
 
-use App\Models\User;
-use App\Models\Paciente;
-use App\Models\Poliza; // Asegúrate de importar esto
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use App\Models\Paciente;
+use App\Models\Poliza;
 
 Route::get('/', function () {
     return view('welcome');
@@ -13,24 +14,30 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     $user = Auth::user();
     
-    // Intenta buscar el paciente, si no existe, crea un objeto vacío para no romper la vista
+    // Si el registro falló antes, esto evita que la pantalla se ponga blanca.
+    // Usamos '??' para crear datos falsos en memoria si la base de datos está vacía.
+    
     $paciente = $user->paciente ?? new Paciente([
-        'ubicacion_zona' => 'No Definida',
-        'id' => 'Pendiente'
+        'ubicacion_zona' => 'Sin Zona',
+        'id' => '---'
     ]);
 
-    // LÓGICA DE SEGURIDAD PARA LA DEMO:
-    // Intenta buscar una póliza real en la BD.
-    // Si NO existe, crea una "en el aire" (new Poliza) para que la tarjeta se vea bonita y no dé error.
-    $poliza = $user->polizas()->first() ?? new Poliza([
-        'nombre_plan' => 'Plan Básico (Demo)',
-        'costo' => 15.00,
-        'cobertura' => 500.00,
-        'estado' => 'activa',
+    // Lógica para la Póliza (Requisito RF.2.0 del PDF)
+    $poliza = $user->polizas()->where('estado', 'activa')->first() ?? new Poliza([
+        'nombre_plan' => 'Plan Básico (No Activo)',
+        'costo' => 0.00,
+        'cobertura' => 0.00,
+        'estado' => 'inactiva',
         'created_at' => now()
     ]);
 
     return view('dashboard', compact('user', 'paciente', 'poliza'));
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
 require __DIR__.'/auth.php';
